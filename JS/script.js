f2aaa5b5433318d37101c40b9963e16718f91ac4
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-05 15:54:04 +0200

    password generator

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,18 @@
-/*Chiedi all’utente il suo nome,
+/*
+Chiedi all’utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 poi chiedi la sua età
 Infine scrivi sulla pagina nomecognomecolorepreferito21 (dove il 21 è in questo caso l'età)
 */
+const userName = prompt('What is your name ?');
+const userSurname = prompt('What is your surname ?');
+const userFavColor = prompt('What is your favorite color ?');
+const userAge = prompt('How old are you ?');
+const welcome = `
+${userName}${userSurname}${userFavColor}${userAge}
+`;
+
+const element = document.getElementById('password');
+
+element.innerHTML = welcome;
